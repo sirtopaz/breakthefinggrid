5005dff2843700b864523f93aaf87761ea6c21cf
sgervais <sgervais@hubspot.com>
2016-04-04 20:30:58 -0400

    added student pictures, updated animations

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,27 +2,27 @@
 $(function() {
   $(".student .detail").on({
     click: function() {
-      $(".student .detail").not( this ).addClass( "animated hinge" );
+      $(".student .detail").not( this ).addClass( "animated zoomOut" );
       $(".screen").removeClass( "hidden" );
     },
     mouseenter: function() {
-      $( this ).removeClass( "animated rollIn" ).addClass( "animated tada" ).focus();
+      $( this ).removeClass( "animated zoomIn" ).addClass( "animated tada" ).focus();
     },
     mouseleave: function() {
-      $( this ).removeClass( "animated rollIn" ).removeClass( "animated tada" ).blur();
+      $( this ).removeClass( "animated zoomIn" ).removeClass( "animated tada" ).blur();
     },
     focus: function() {
-      $( this ).removeClass( "animated rollIn" ).addClass( "animated tada" );
+      $( this ).removeClass( "animated zoomIn" ).addClass( "animated tada" );
     },
     blur: function() {
-      $( this ).removeClass( "animated rollIn" ).removeClass( "animated tada" );
+      $( this ).removeClass( "animated zoomIn" ).removeClass( "animated tada" );
     }
   });
 
   $( ".screen .close").on( {
     click: function() {
       $(".screen").addClass("hidden");
-      $(".student .detail").removeClass( "animated hinge" ).addClass( "animated rollIn" );
+      $(".student .detail").removeClass( "animated zoomOut" ).addClass( "animated zoomIn" );
     }
   });
 });
